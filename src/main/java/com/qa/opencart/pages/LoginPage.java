00bd68d0db53783828c22908b39ad4d951789de6
Rosario <rpregis2@gmail.com>
2024-04-25 00:58:41 -0400

    first commit

---
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -0,0 +1,65 @@
+package com.qa.opencart.pages;
+
+
+import java.sql.Time;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.qa.opencart.constants.AppConstants;
+import com.qa.opencart.utils.ElementUtil;
+import com.qa.opencart.utils.TimeUtil;
+
+public class LoginPage {
+
+	//Page class/Page Library/Page Object
+	private WebDriver driver;
+	private ElementUtil eleUtil;
+
+	// 1. Private By Locators
+	private By emailId = By.id("input-email");
+	private By password = By.id("input-password");
+	private By loginButton = By.xpath("//input[@value='Login']");
+	private By forgotPWdLink = By.linkText("Forgotten Password");
+	private By registerLink = By.linkText("Register");
+
+
+	// 2. Public Page Class Const...
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+		eleUtil = new ElementUtil(driver);
+	}
+
+	// 3. Public Page Actions/Method
+	public String getLoginPageTitle() {
+		String title = eleUtil.waitForTitleIs(AppConstants.LOGIN_PAGE_TITLE, TimeUtil.DEFAULT_MEDIUM_TIME);
+		System.out.println("login page title : " + title);
+		return title;
+	}
+
+	public String getLoginPageURL() {
+		String url = eleUtil.waitForURLContains(AppConstants.LOGIN_PAGE_URL_FRACTION, TimeUtil.DEFAULT_MEDIUM_TIME);
+		System.out.println("login page url : " + url);
+		return url;
+	}
+
+	public boolean isForgotPwdLinkExist() {
+		return eleUtil.isElementDisplayed(forgotPWdLink);
+	}
+
+	public AccountsPage doLogin(String username, String pwd) {
+		System.out.println("user creds: " + username + " : " + pwd);
+		eleUtil.waitForElementVisible(emailId, TimeUtil.DEFAULT_LONG_TIME).sendKeys(username);
+		eleUtil.doSendKeys(password, pwd);
+		eleUtil.doClick(loginButton);
+		return new AccountsPage(driver);
+	}
+	
+	
+	public RegistrationPage navigateToRegisterPage() {
+		eleUtil.waitForElementVisible(registerLink, TimeUtil.DEFAULT_LONG_TIME).click();
+		return new RegistrationPage(driver);
+	}
+	
+
+}
---
+++ src/main/java/com/qa/opencart/utils/StringUtils.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.utils;
+
+
+public class StringUtils {
+
+	public static String getRandomEmailId() {
+		String emailId = "decbatch" + System.currentTimeMillis() + "@opencart.com";
+		return emailId;
+	}
+
+	
+
+}
